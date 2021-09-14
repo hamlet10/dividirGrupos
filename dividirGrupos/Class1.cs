8c2eac022ced99c24af4fa995ce519c1758eeabf
hamlet10 <hpsanchez@live.com>
2021-09-13 21:00:52 -0400

    first_test_past

--- dividirGrupos/Class1.cs
+++ dividirGrupos/Class1.cs
@@ -6,6 +6,9 @@ namespace dividirGrupos
     {
 
         public static string Input_Validator(string? input){
+            if(String.IsNullOrEmpty(input)){
+                throw new Exception();
+            }
             return "hello world";
         }
     }
